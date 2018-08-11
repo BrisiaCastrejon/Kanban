f038198aabe3c4d8f04f61484eedf09986836393
BrisiaCastrejon <brissmonse@gmail.com>
2018-08-11 16:36:19 -0500

    Vistas

---
+++ src/js/view.js
@@ -0,0 +1,21 @@
+let containerOne = document.getElementsByClassName('row');
+let containerTwo = document.querySelector('.container');
+containerTwo.addEventListener('dragover', listMoveContainer, false );
+containerTwo.addEventListener('dragleave', listMoveContainerEnd, false);
+containerOne.addEventListener('dragstart', listMoveStar, false);
+containerOne.addEventListener('dragend', listMoveEnd, false);
+const listMoveContainer = (event) => {
+event.preventDefault();
+containerTwo.classList.add('over');
+return false
+}
+const listMoveContainerEnd = (event) => {
+
+}
+const listMoveStar = (event) => {
+  containerOne.style.display = 'none';
+}
+const listMoveEnd = (event) => {
+  containerOne.style.display = 'none';
+}
+
